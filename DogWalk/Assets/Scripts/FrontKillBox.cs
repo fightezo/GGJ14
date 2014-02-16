4c357b38471f4bd41206c0f6dba19ee00d84aea3
Douglas Wong <fight320@hotmail.com>
2014-02-15 23:59:53 -0800

    cleaned some files and worked on distraction destroyer

--- DogWalk/Assets/Scripts/FrontKillBox.cs
+++ DogWalk/Assets/Scripts/FrontKillBox.cs
@@ -1,26 +1,33 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class FrontKillBox : MonoBehaviour {
+//
+//	// Use this for initialization
+//	void Start (){
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update (){
+//	
+//	}
+//
+//	void OnTriggerEnter(Collider other){
+//		if (other.gameObject.tag == "distraction")
+//		{
+//			Destroy(gameObject);
+//		}
+//		
+//	}
+//
+//}
+
+using UnityEngine;
 using System.Collections;
 
-public class FrontKillBox : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	void OnTriggerEnter(Collider other)
-	{
-		Debug.Log (other.gameObject);
-		if (other.gameObject.tag == "distraction")
-		{
-			Destroy(gameObject);
-
-		}
-		
+public class FrontKillBox : MonoBehaviour{
+	void OnTriggerEnter(Collider other){
+		Destroy(other.gameObject);
 	}
 }
